acf74872c8a9b5335f744239a6d7b1f3fd5e7f23
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-02-13 16:35:46 +0100

    Ex 4 finit

--- TP/src/tp1/TestHBox.java
+++ TP/src/tp1/TestHBox.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -27,10 +29,13 @@ public class TestHBox extends Application {
     	Button btn1 = new Button("Un");
         Button btn2 = new Button("Deux");
         Button btn3 = new Button("Trois");
-        Button btn4 = new Button("Quatre");
-        Button btn5 = new Button("Cinq");
-        Button btn6 = new Button("Six");
         
+        
+        btn2.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(btn2,Priority.ALWAYS);
+        
+        Region ressort = new Region();
+        HBox.setHgrow(ressort,Priority.ALWAYS);
         
         
         
@@ -39,15 +44,12 @@ public class TestHBox extends Application {
         
         HBox root = new HBox();
         
-        root.getChildren().addAll(btn1, btn2, btn3, btn4, btn5, btn6);
-        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(btn1, ressort, btn2, btn3);
+        root.setAlignment(Pos.TOP_CENTER);
         
-        HBox.setMargin(btn1, new Insets(0, 50, 0, 0 ));
-        HBox.setMargin(btn2, new Insets(0, 0, 20, 0));
-        HBox.setMargin(btn3, new Insets(0, 0, 40, 0));
-        HBox.setMargin(btn4, new Insets(0, 0, 60, 0));
-        HBox.setMargin(btn5, new Insets(0, 0, 80, 0));
-        HBox.setMargin(btn6, new Insets(0, 0, 0, 50 ));
+//        HBox.setMargin(btn1, new Insets(0, 50, 0, 0 ));
+//        HBox.setMargin(btn2, new Insets(0, 0, 20, 0));
+        
         
         
         primaryStage.setScene(new Scene(root, 300, 200));
