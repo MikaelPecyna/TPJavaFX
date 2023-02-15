62e19ccb9c35829c9a2fd02d086b9da963989401
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-02-15 12:04:40 +0100

    Ex5 : MyButton : fait !

---
+++ TP/src/tp4/ex5/MyButton.java
@@ -0,0 +1,34 @@
+package tp4.ex5;
+
+import javafx.scene.control.Button;
+
+public class MyButton extends Button {
+	private int line ;
+	private int column;
+	
+	public MyButton(int line, int column) {
+		super(); 
+		this.column = column;
+		this.line = line; 
+		this.setText("(" + this.line + ";" + this.column + ")");
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+
+}
---
+++ TP/src/tp4/ex5/MyButtonTest.java
@@ -0,0 +1,5 @@
+package tp4.ex5;
+
+public class MyButtonTest {
+
+}
